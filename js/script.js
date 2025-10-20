1c587dcd993be72ae71405cac141abeb04197733
gabrielstoute <gabri.andres23@gmail.com>
2025-10-20 13:06:33 +0200

    js update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* js */
+
+/* mensaje d bienvenida  */
+window.addEventListener("load", function () {
+  alert("🎶 Bienvenido al NEW YORK SOUND FEST 2025 🎵");
+  console.log("La página ha cargado correctamente");
+});
