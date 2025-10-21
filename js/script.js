3802decd4322b301b52d721086cfb3ea5d0252f7
gabrielstoute <gabri.andres23@gmail.com>
2025-10-21 08:39:22 +0200

    tickets

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ function mostrarPrograma(dia) {
   alert("Has hecho clic en el programa del día: " + dia);
   console.log("Usuario consultó el programa del " + dia);
 }
+/* contacto formulario */
+function abrirFormulario() {
+  alert("Formulario de contacto abierto");
+}
+/* menu */
+let menuVisible = false;
+function mostrarMenu() {
+  const nav = document.querySelector("nav > ul.menu_pr");
+  if (menuVisible) {
+    nav.style.display = "none";
+    menuVisible = false;
+  } else {
+    nav.style.display = "flex";
+    menuVisible = true;
+  }
+}
+/* boton de entradas */
+const botonEntradas = document.querySelector(".hero button");
+if (botonEntradas) {
+  botonEntradas.addEventListener("click", function () {
+    window.location.href = "tickets.html";
+  });
+}
